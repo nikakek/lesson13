d927d747b76226f6855cbed848335705abff27f6
nikakek <nikakekenadze01@gmail.com>
2023-04-06 17:24:33 +0400

    lesson13

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("text from js file");
+
+let myComputerManufacturer= "acer";
+let myComputerMemory= "8gb ram"
+let myComputerOS= "windows10";
+let computerBuyingYear= 2015;
+let currentYear= 2023;
+
+const manufacturer= "my computer is "+ myComputerManufacturer;
+console.log(manufacturer);
+
+const memory=" it runs on "+ myComputerMemory;
+console.log(memory);
+
+const usingTime= currentYear-computerBuyingYear;
+console.log("and I've been using it for "+usingTime +" years");
